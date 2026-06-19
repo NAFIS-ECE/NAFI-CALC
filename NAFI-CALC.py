427afa55dd632690cc43bb438d9cb94f00f6066d
P S SHEIK MD NAFIS <nafisriyaz19@gmail.com>
2026-06-19 23:23:01 +0530

    Add zero division check to calculator
    ADDED A EXTENDED VERSION OF NAFI-CALC TO GET A RID FROM THE CRASH

--- NAFI-CALC.py
+++ NAFI-CALC.py
@@ -14,7 +14,10 @@ elif operator == "*":
     print("Result =", num1 * num2)
 
 elif operator == "/":
-    print("Result =", num1 / num2)
+    if num2 == 0:
+        print("Error: Cannot divide by zero")
+    else:
+        print("Result =", num1 / num2)
 
 else:
     print("Invalid operator")
